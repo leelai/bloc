3327b1d830f0045b403be671808b08d99e2a8265
leelai <leelai.tw@gmail.com>
2021-11-17 23:46:26 +0800

    add sn

--- examples/flutter_login/lib/utils/utils.dart
+++ examples/flutter_login/lib/utils/utils.dart
@@ -2,7 +2,8 @@ import 'package:platform_device_id/platform_device_id.dart';
 
 List<String> systemGUIDs = [
   '899369E3-745A-5617-A837-3158E968D793',
-  '46D00CC8-2C8A-4E49-976D-5F73396C1C05'
+  '46D00CC8-2C8A-4E49-976D-5F73396C1C05',
+  '4C4C4544-0046-5710-8056-C4C04F464332'
 ];
 
 class Util {
